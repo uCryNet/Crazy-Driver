97486d9d43f81af278d2177bbc7b333bfe09bb9f
uCryNet <ucrynet@gmail.com>
2026-09-06 00:15:13 +0300

    add start logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -21,6 +22,35 @@ public class GameManager : MonoBehaviour
     private bool levelCompleted;
     
     private const string FinishText = "FINISH!";
+    private const string StartText = "GO!";
+    private const int CountdownFrom = 3;
+    private const float CountdownStep = 1f;
+    private const float GoMessageTime = 2f;
+
+    private void Start()
+    {
+        StartCoroutine(CountdownRoutine());
+    }
+
+    private IEnumerator CountdownRoutine()
+    {
+        info.gameObject.SetActive(true);
+
+        player.constraints = RigidbodyConstraints.FreezeAll;
+
+        for (int count = CountdownFrom; count > 0; count--)
+        {
+            info.text = count.ToString();
+            yield return new WaitForSeconds(CountdownStep);
+        }
+
+        player.constraints = RigidbodyConstraints.None;
+
+        info.text = StartText;
+        yield return new WaitForSeconds(GoMessageTime);
+
+        info.gameObject.SetActive(false);
+    }
 
     private void FixedUpdate()
     {
@@ -40,6 +70,9 @@ public class GameManager : MonoBehaviour
         if (stoppedTime >= requiredStopTime)
         {
             levelCompleted = true;
+
+            StopAllCoroutines();
+
             info.text = FinishText;
             info.gameObject.SetActive(true);
         }
